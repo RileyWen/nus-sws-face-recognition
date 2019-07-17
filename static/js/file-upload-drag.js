1d57c39e75628847586cd281cf9836d18f438ea3
Bavichi <909452380@qq.com>
2019-07-17 16:00:58 +0800

    progress+video

--- static/js/file-upload-drag.js
+++ static/js/file-upload-drag.js
@@ -1,11 +1,9 @@
 let file;
-let img_show = false;
+let video_show = false;
 
 function file_upload() {
-    if (img_show) {
-        $("#result-img-card").fadeOut("4000");
-        img_show = false;
-    }
+    file = $('#selected-file')[0].files[0];
+    console.log(file);
 
     if (file) {
         let form = new FormData();
@@ -14,40 +12,51 @@ function file_upload() {
         let file_reader = new FileReader();
         file_reader.onloadend = function () {
             dataURL = file_reader.result;
-            form.append('image', dataURL);
+            console.log(dataURL);
+            form.append('file', file);
+            
 
             let xmlHTTP = new XMLHttpRequest();
             xmlHTTP.onreadystatechange = function () {
                 if (xmlHTTP.readyState === 4) {
                     if (xmlHTTP.status === 200) {
+                        console.log('上传成功！');
                         let resp_text = xmlHTTP.responseText;
                         console.log(xmlHTTP.responseText);
-
-                        let img = document.getElementById("result-img");
-                        img.src = resp_text;
-
-                        if (!img_show) {
-                            $("#result-img-card").delay(200).fadeIn("4000");
-                            img_show = true;
-                        }
+                        let video = document.getElementById("result-video");
+                        video.src = resp_text;
                     } else {
+                        console.log("上传失败！");
                         console.log(xmlHTTP.responseText);
                     }
                 }
             };
-            xmlHTTP.open('post', 'UploadPic');
+            xmlHTTP.open('post', '');   
             // alert(dataURL;
             // xmlHTTP.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            console.log(form);
             xmlHTTP.send(form);
         };
         file_reader.readAsDataURL(file);
+
+        //进度条
+        file_reader.addEventListener("progress",e=>{
+            document.querySelector('.progress').style.width=(e.loaded/e.total)*100+'%'
+        });
+        file_reader.addEventListener('error',err=>{
+            throw err;
+        });
     } else {
-        alert('You must select an image!');
+        alert('You must select an video!');
     }
+
+
 }
 
 function filename_update() {
+    
     file = $('#selected-file')[0].files[0];
+    console.log(file);
     let file_label = document.getElementById('file-label');
     if (file) {
         file_label.textContent = file.name;
